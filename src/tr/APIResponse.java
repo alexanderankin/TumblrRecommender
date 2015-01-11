c990c0c30e08d698a611ab82b072001a8eafbd7f
Dave <alexanderankin@gmail.com>
2015-01-10 21:05:51 -0500

    Got JSON Strings for both blog information and individual posts, to put into an array

--- src/tr/APIResponse.java
+++ src/tr/APIResponse.java
@@ -1,59 +1,108 @@
 package tr;
 
-public class APIResponse{
+public class APIResponse {
 	private String content = "";
 	private String action = "";
-	private String thisisapartofthejsonineedtosleep = "";
-	
-	public APIResponse (String a, String b){
+	private String blogInfo = "";
+	private String posts = "";
+
+	// private String[] postStrings = new String[20];
+
+	public APIResponse(String a, String b) {
 		setContent(a);
 		setAction(b);
-		thisisapartofthejsonineedtosleep = getResponse(content);
-		System.out.println(thisisapartofthejsonineedtosleep);
-		
+		System.out.println(blogInfo);
+		substringResponse(a, b);
 	}
-	public String getResponse (String a){
-		int srtLoc = a.indexOf("blog")+"blog".length()+2;
+
+	public void substringResponse(String a, String b) {
+
 		int parenCount = 0;
 		int endLoc = 0;
-		searchrestofstring: for (int i = srtLoc; i < a.length(); i++) {
-			switch (a.charAt(i)) {
-			case '{':
-				parenCount++;
-				break;
-			case '}':
-				parenCount--;
-				break;
-			default:
-				break;
+		int srtLoc;
+		switch (b) {
+		case "blog":
+			srtLoc = a.indexOf(b) + b.length() + 2;
+			searchrestofstring: for (int i = srtLoc; i < a.length(); i++) {
+				switch (a.charAt(i)) {
+				case '{':
+					parenCount++;
+					break;
+				case '}':
+					parenCount--;
+					break;
+				default:
+					break;
+				}
+				if (parenCount == 0) {
+					endLoc = i + 1;
+					break searchrestofstring;
+				}
 			}
-			if (parenCount == 0) {
-				endLoc = i+1;
-				break searchrestofstring;
+			blogInfo = a.substring(srtLoc, endLoc);
+			break;
+		case "posts":
+			srtLoc = a.indexOf(b, a.indexOf(b) + 1) + b.length() + 2;
+			parenCount = 0;
+			endLoc = 0;
+			for (int i = srtLoc; i < a.length() - 4; i++) {
+				switch (a.charAt(i)) {
+				case '[':
+					parenCount++;
+					break;
+				case ']':
+					parenCount--;
+					break;
+				default:
+					break;
+				}
+				if (parenCount == 0) {
+					endLoc = i + 1;
+					posts = a.substring(srtLoc, endLoc);
+					separateposts();
+				}
 			}
+
+			break;
+		default:
+			break;
 		}
-		return a.substring(srtLoc,endLoc);
+
 	}
+
+	private void separateposts() {
+		int j = 0; // count posts, keep track of which element of the array to add to.
+		for (int i = 0; i < posts.length(); i++) {
+
+		}
+		System.out.println("hello " + j);
+	}
+
 	/**
 	 * @return the action
 	 */
 	public String getAction() {
 		return action;
 	}
+
 	/**
-	 * @param action the action to set
+	 * @param action
+	 *            the action to set
 	 */
 	public void setAction(String action) {
 		this.action = action;
 	}
+
 	/**
 	 * @return the content
 	 */
 	public String getContent() {
 		return content;
 	}
+
 	/**
-	 * @param content the content to set
+	 * @param content
+	 *            the content to set
 	 */
 	public void setContent(String content) {
 		this.content = content;
